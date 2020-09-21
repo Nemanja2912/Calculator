f0a0fc0770662f77bd8a2974bdc5dcc991a41065
Anonim2912 <nemanja.mijailovic2912@hotmail.com>
2020-09-21 20:21:00 +0200

    Updates

---
+++ precache-manifest.f5ba56e91fd75ca37b47af8c19f0205a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4bb079acc91a636ed9b116db1f1558e",
+    "url": "/Calculator/index.html"
+  },
+  {
+    "revision": "e750303d76e8d671e452",
+    "url": "/Calculator/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fab3c1cf399127029d0f",
+    "url": "/Calculator/static/js/2.6cb6d0fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Calculator/static/js/2.6cb6d0fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e750303d76e8d671e452",
+    "url": "/Calculator/static/js/main.90140e8c.chunk.js"
+  },
+  {
+    "revision": "3ce7024b533a51d663dd",
+    "url": "/Calculator/static/js/runtime-main.c1781e1b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Calculator/static/media/logo.5d5d9eef.svg"
+  }
+]);
