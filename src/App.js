a1f3c79775f2167a5e786357d249b9c9cac4bbcc
Anonim2912 <nemanja.mijailovic2912@hotmail.com>
2020-09-29 17:17:45 +0200

    New Version

--- src/App.js
+++ src/App.js
@@ -1,44 +1,9 @@
 import React, { Component } from "react";
-import Body from "./components/body";
-import Screen from "./components/screen";
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      preview: "",
-      display: "",
-      height: window.innerHeight,
-    };
-  }
-
-  getData = (data, calculus) => {
-    let expo;
-    String(data).length > 9
-      ? (expo = Number(data).toExponential(5))
-      : (expo = data);
-
-    let preview = String(calculus).split(" ");
-
-    for (let i = 0; i < preview.length; i++) {
-      if (String(preview[i]).length > 9) {
-        preview[i] = Number(preview[i]).toExponential(5);
-      }
-    }
-
-    this.setState({
-      display: expo,
-      preview: preview,
-    });
-  };
-
+  state = {};
   render() {
-    return (
-      <div style={{ height: `${this.state.height}px` }} className="app">
-        <Screen display={this.state.display} preview={this.state.preview} />
-        <Body getData={this.getData} />
-      </div>
-    );
+    return <h1>Hello</h1>;
   }
 }
 
